a8be2c7abf30391a5a67d353b78d81b3f0e80c7d
Mohamed Burhan <bern2727@gmail.com>
2022-10-13 21:37:32 +0800

    1. update the lesson planner 2. implement the useState

--- todo-app/src/pages/LandingPage.jsx
+++ todo-app/src/pages/LandingPage.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const LandingPage = () => {
-  return (
-    <div>
-      <h1>Hello World!!</h1>
-    </div>
-  )
-}
 
-export const SafwanPage = () => {
+  /**
+   * what is useState?
+   * - useState is a hook 
+   * - it is used to hold your value that you want to mutate
+  */
+
+  // THIS IS CANNOT
+  // let x = 1
+  // x = 2
+  // You can't MUTATE variable
+
+  const [firstName, setFirstName] = useState("-")
+
   return (
     <div>
-      <h1>Hello World Safwan!!</h1>
+      <h1>Nama saya ialah: {firstName}</h1>
+
+      <input type="text" onChange={ (evt) => setFirstName(evt.target.value) } />
     </div>
   )
 }

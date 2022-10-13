98956f7c1d58782d5e024b531469e3e1dfcc06e4
Mohamed Burhan <bern2727@gmail.com>
2022-10-13 21:04:59 +0800

    1. creat a landing page 2. use the landing page as our primary landing page

---
+++ todo-app/src/pages/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const LandingPage = () => {
+  return (
+    <div>
+      <h1>Hello World!!</h1>
+    </div>
+  )
+}
+
+export const SafwanPage = () => {
+  return (
+    <div>
+      <h1>Hello World Safwan!!</h1>
+    </div>
+  )
+}

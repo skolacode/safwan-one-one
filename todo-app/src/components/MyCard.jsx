618170aa2b1b5097394ef5e041d8c608f600b0cb
Mohamed Burhan <bern2727@gmail.com>
2022-10-17 21:35:41 +0800

    1. update the lesson plan 2. learn a normal style and module style

--- todo-app/src/components/MyCard.jsx
+++ todo-app/src/components/MyCard.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import myCardStyle from '../styles/MyCard.module.css'
+import newCardStyle from '../styles/NewCard.module.css'
 
 // de-structure
 export const MyCard = (props) => {
@@ -6,9 +8,9 @@ export const MyCard = (props) => {
   const { children } = props
 
   return (
-    <>
+    <div className={myCardStyle.container}>
       <div>This is Card component</div>
       {children}
-    </>
+    </div>
   )
 }

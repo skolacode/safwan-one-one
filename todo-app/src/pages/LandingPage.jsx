af2c9b5a2f317961c0e4c729c90fd8c5123e881b
Mohamed Burhan <bern2727@gmail.com>
2022-10-31 22:20:48 +0800

    1. implement save todo using reducer

--- todo-app/src/pages/LandingPage.jsx
+++ todo-app/src/pages/LandingPage.jsx
@@ -1,11 +1,17 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import {MyCard} from '../components/MyCard'
 import landingPageStyle from '../styles/LandingPage.module.css'
 
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { landingPageReducer, landingPageState } from '../reducers/LandingPageReducer';
 
 export const LandingPage = () => {
+
+  const [state, dispatch] = useReducer(landingPageReducer, landingPageState);
+
+  // state.todo = '' > after update using dispatch > 'no longer empty'
+  // landingPageState.todo = ''
 
   /**
    * what is useState?
@@ -18,66 +24,6 @@ export const LandingPage = () => {
   // x = 2
   // You can't MUTATE variable
 
-  const [todo, setTodo] = useState('')
-  const [todoList, setTodoList] = useState([])
-  const [doneTodoList, setDoneTodoList] = useState([])
-
-  const onSave = () => {
-
-    if(todo === '') {
-      return 
-    }
-
-    // using spread operator to deep clone
-    const cloneTodoList = [...todoList]
-
-    cloneTodoList.push(todo)
-
-    setTodoList(cloneTodoList)
-
-    setTodo('')
-  }
-
-  const deleteTodo = (index) => {
-    console.log('get index: ', index)
-    const cloneTodoList = [...todoList]
-    
-    cloneTodoList.splice(index, 1)
-
-    setTodoList(cloneTodoList)
-  }
-
-  const doneTodo = (index) => {
-    console.log('index:', index)
-    
-    const doneTodo = todoList[index]
-    
-    // add into doneTodoList
-    const cloneDoneTodoList = [...doneTodoList]
-    cloneDoneTodoList.push(doneTodo)
-    setDoneTodoList(cloneDoneTodoList)
-    
-    // delete from original list
-    deleteTodo(index)
-  }
-
-  const deleteDoneTodo = (index) => {
-    console.log('get index: ', index)
-    const cloneTodoList = [...doneTodoList]
-    
-    cloneTodoList.splice(index, 1)
-
-    setDoneTodoList(cloneTodoList)
-  }
-
-  const editTodo = (index) => {
-    // get the clicked value and set to todo variable
-    setTodo(todoList[index])
-    // delete from todo list
-    deleteTodo(index)
-  }
-
-
   return (
     <div className={landingPageStyle.container}>
 
@@ -88,22 +34,29 @@ export const LandingPage = () => {
           label="Todo" 
           variant="outlined" 
           type="text" 
-          value={todo} 
+          value={state.todo} 
           size="normal"
-          onChange={(evt) => setTodo(evt.target.value)}
+          onChange={(evt) => dispatch({ type: 'TODO', payload: { todo: evt.target.value } })}
           style={{ width: '81%' }}
         />
       
-        <Button variant="contained" size="large" sx={{ marginLeft: 1, height: 53 }} onClick={onSave}>Save</Button>
+        <Button 
+          variant="contained" 
+          size="large" 
+          sx={{ marginLeft: 1, height: 53 }} 
+          onClick={() => dispatch({ type: 'SAVE' })}
+        >
+          Save
+        </Button>
       </div>
 
       {
-        todoList.map((each, index) => (
-          <MyCard key={index} index={index} description={each} deleteFunc={deleteTodo} doneFunc={doneTodo} editFunc={editTodo} />
+        state.todoList.map((each, index) => (
+          <MyCard key={index} index={index} description={each} />
         ))
       }
 
-      {
+      {/* {
         doneTodoList.length > 0 &&
         <>
           <h3>Done List</h3>
@@ -113,7 +66,7 @@ export const LandingPage = () => {
             ))
           }
         </>
-      }
+      } */}
 
       {/* 
       1. Self closing jika tiada children, 
---
+++ todo-app/src/reducers/LandingPageReducer.js
@@ -0,0 +1,78 @@
+const onSave = (state) => {
+
+  if(state.todo === '') {
+    return 
+  }
+
+  const cloneTodoList = [...state.todoList]
+
+  cloneTodoList.push(state.todo)
+
+  return {
+    ...state, // todoList: [], todo: '', completedTodoList: []
+    todo: '',
+    todoList: cloneTodoList
+  }
+}
+
+// const deleteTodo = (index) => {
+//   console.log('get index: ', index)
+//   const cloneTodoList = [...todoList]
+  
+//   cloneTodoList.splice(index, 1)
+
+//   setTodoList(cloneTodoList)
+// }
+
+// const doneTodo = (index) => {
+//   console.log('index:', index)
+  
+//   const doneTodo = todoList[index]
+  
+//   // add into doneTodoList
+//   const cloneDoneTodoList = [...doneTodoList]
+//   cloneDoneTodoList.push(doneTodo)
+//   setDoneTodoList(cloneDoneTodoList)
+  
+//   // delete from original list
+//   deleteTodo(index)
+// }
+
+// const deleteDoneTodo = (index) => {
+//   console.log('get index: ', index)
+//   const cloneTodoList = [...doneTodoList]
+  
+//   cloneTodoList.splice(index, 1)
+
+//   setDoneTodoList(cloneTodoList)
+// }
+
+// const editTodo = (index) => {
+//   // get the clicked value and set to todo variable
+//   setTodo(todoList[index])
+//   // delete from todo list
+//   deleteTodo(index)
+// }
+
+export const landingPageState = {
+  todo: '',
+  todoList: [],
+  doneTodoList: []
+}
+
+export const landingPageReducer = (state, action) => {
+  
+  switch (action.type) {
+    case 'TODO': {
+      return {
+        ...state, // spread operator
+        todo: action.payload.todo
+      }
+    }
+    case 'SAVE': {
+      return onSave(state)
+    }
+    default:
+      return true;
+  }
+}
